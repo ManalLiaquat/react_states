b440d19c2d743aa70d3a4d8193535ed1aebd7e92
ManalLiaquat <m.manalliaquat@gmail.com>
2018-08-19 09:35:02 +0500

    creating functions in react

--- src/App.js
+++ src/App.js
@@ -8,30 +8,54 @@ class App extends Component {
     super()
     this.state = {
       date: new Date().toLocaleString(),
-      count: 0
+      count: 0,
+      text: 'I love Pakistan',
+      condition: true
     }
   }
 
   counter(){
     this.setState({
       date: new Date().toLocaleString(),
-      count: ++this.state.count
+      count: ++this.state.count,
+      text: this.state.condition ? "Hello World" : 'I love Pakistan',
+      condition: !this.state.condition
     })
+  }
+
+  renderHeader(){
+    return(
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+    )
+  }
+
+  renderBody(){
+    return(
+      <div>
+        <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>
+        <button onClick={() => this.counter()}>click</button>
+        <p>Date: {this.state.date}</p>
+        <p>Count: {this.state.count}</p>
+        <p>{this.state.text}</p>
+      </div>
+    )
+  }
+
+  renderFooter(){
+    return(
+      <h1>This is a footer</h1>
+    )
   }
   
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={()=> this.counter()}>click</button>
-        <p>Date: {this.state.date}</p>
-        <p>Count: {this.state.count}</p>
+        {this.renderHeader()}
+        {this.renderBody()}
+        {this.renderFooter()}
       </div>
     );
   }
